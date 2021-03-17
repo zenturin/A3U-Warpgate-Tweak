zenturin/A3U-Warpgate-Tweak

commit 237b927d04d6600968008544a7979a1b74346fa5

Fixed paradrops, distances and unit counts (#1781)
## What type of PR is this.
1. [X] Bug
2. [ ] Change
3. [ ] Enhancement

### What have you changed and why?
Information:
The new support QRFs tried to paradrop entire squads near the AI limit, which then resulted into paradropping one guy.
Readded the block, the ensures that this isnt happening. Also decreased the radius in which they can drop, so it doesnt take forever for them to arrive.

### Please specify which Issue this PR Resolves.
None opened for that

### Please verify the following and ensure all checks are completed.

1. [X] Have you loaded the mission in LAN host?
2. [X] Have you loaded the mission on a dedicated server?

### Is further testing or are further changes required?
1. [X] No
2. [ ] Yes (Please provide further detail below.)

### How can the changes be tested?
Steps: Was on the test server during the large test, no further testing required

********************************************************
Notes:

* Fixed paradrops, distances and unit counts

* Fixed based on Hakons comment, untested

A3-Antistasi/functions/CREATE/fn_createAttackVehicle.sqf
@@ -32,7 +32,9 @@ private _crewGroup = [_side, _vehicle] call A3A_fnc_createVehicleCrew;
 [_vehicle, _side] call A3A_fnc_AIVEHinit;
 
 private _cargoGroup = grpNull;
-if ((([_vehicleType, true] call BIS_fnc_crewCount) - ([_vehicleType, false] call BIS_fnc_crewCount)) > 0) then
+private _spawnPerformed = false;
+private _expectedCargo = ([_vehicleType, true] call BIS_fnc_crewCount) - ([_vehicleType, false] call BIS_fnc_crewCount);
+if (_expectedCargo > 0) then
 {
     //Vehicle is able to transport units
     private _groupType = if (_typeOfAttack == "Normal") then
@@ -50,20 +52,46 @@ if ((([_vehicleType, true] call BIS_fnc_crewCount) - ([_vehicleType, false] call
             if (_side == Occupants) then {groupsNATOAT} else {groupsCSATAT}
         };
     };
-    _cargoGroup = [_posOrigin, _side, _groupType] call A3A_fnc_spawnGroup;
+
+    private _allUnits = {(local _x) and (alive _x)} count allUnits;
+
+    private _allUnitsSide = 0;
+    private _maxUnitsSide = maxUnits;
+    if (gameMode < 3) then
     {
-        _x assignAsCargo _vehicle;
-        _x moveInCargo _vehicle;
-        if !(isNull objectParent _x) then
+        _allUnitsSide = {(local _x) and (alive _x) and (side group _x == _sideX)} count allUnits;
+        _maxUnitsSide = round (maxUnits * 0.7);
+    };
+
+    if ((_allUnits + _expectedCargo) <= maxUnits  && (_allUnitsSide + _expectedCargo) <= _maxUnitsSide) then
+    {
+        _spawnPerformed = true;
+        _cargoGroup = [_posOrigin, _side, _groupType] call A3A_fnc_spawnGroup;
         {
-            [_x] call A3A_fnc_NATOinit;
-            _x setVariable ["originX", _markerOrigin];
-        }
-        else
-        {
-            deleteVehicle _x;
-        };
-    } forEach units _cargoGroup;
+            _x assignAsCargo _vehicle;
+            _x moveInCargo _vehicle;
+            if !(isNull objectParent _x) then
+            {
+                [_x] call A3A_fnc_NATOinit;
+                _x setVariable ["originX", _markerOrigin];
+            }
+            else
+            {
+                deleteVehicle _x;
+            };
+        } forEach units _cargoGroup;
+    };
+};
+
+if(!_spawnPerformed) exitWith
+{
+    [3, "Unit limit reached, deleting vehicle and crew", _fileName] call A3A_fnc_log;
+    {
+        deleteVehicle _x;
+    } forEach (units _crewGroup);
+    deleteVehicle _vehicle;
+    deleteGroup _crewGroup;
+    objNull;
 };
 
 _landPosBlacklist = [_vehicle, _crewGroup, _cargoGroup, _posDestination, _markerOrigin, _landPosBlacklist] call A3A_fnc_createVehicleQRFBehaviour;

A3-Antistasi/functions/AI/fn_airdrop.sqf
@@ -11,8 +11,8 @@ if (_markerX isEqualType "") then {_positionX = getMarkerPos _markerX};
 _groupPilot = group driver _veh;
 {_x disableAI "TARGET"; _x disableAI "AUTOTARGET"} foreach units _groupPilot;
 _dist = 500;
-_distEng = if (_veh isKindOf "Helicopter") then {1000} else {2000};
-_distExit = if (_veh isKindOf "Helicopter") then {400} else {1000};
+_distEng = if (_veh isKindOf "Helicopter") then {500} else {1000};
+_distExit = if (_veh isKindOf "Helicopter") then {200} else {500};
 _orig = getMarkerPos _originX;
 
 

A3-Antistasi/functions/CREATE/fn_wavedCA.sqf
@@ -586,7 +586,7 @@ while {(_waves > 0)} do
 					}
 				else
 					{
-					_landPos = _posDestination getPos [300, random 360];
+					_landPos = _posDestination getPos [150, random 360];
 					_landPos = [_landPos, 0, 550, 10, 0, 0.20, 0,[],[[0,0,0],[0,0,0]]] call BIS_fnc_findSafePos;
 					if !(_landPos isEqualTo [0,0,0]) then
 						{

commit 030e31f90b63a818a201823c05792ae73294a935

Fix typo breaking transport-vehicles
This typo was introduced by #1781 and led to transport choppers (and others) having no waypoints as it's refering to an undifined variable.
Fixed and tested by Wurzel, committed by me.

A3-Antistasi/functions/CREATE/fn_createAttackVehicle.sqf
@@ -59,7 +59,7 @@ if (_expectedCargo > 0) then
     private _maxUnitsSide = maxUnits;
     if (gameMode < 3) then
     {
-        _allUnitsSide = {(local _x) and (alive _x) and (side group _x == _sideX)} count allUnits;
+        _allUnitsSide = {(local _x) and (alive _x) and (side group _x == _side)} count allUnits;
         _maxUnitsSide = round (maxUnits * 0.7);
     };
 