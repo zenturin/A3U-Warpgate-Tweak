6b9c3b36d126feb657ebe46f61427bd5d548a10a
Bob-Murphy <50651578+Bob-Murphy@users.noreply.github.com>
2022-09-18 21:05:15 +0200

    add expansion info (#2455)
    * add expansion info
    
    * adds small antistasi logo
    
    * typo dix
    
    * "name" update
    
    * additional data and logos
    
    Co-authored-by: Bob-Murphy <Bob-Murphy@users.noreply.github.com>

--- A3A/mod.cpp
+++ A3A/mod.cpp
@@ -1,11 +1,16 @@
-name = "A3 Antistasi";
-author = "A3-Antistasi dev team";
-logo = "";
-logoOver = "";
-logoSmall = "";
-picture = "";
+#include "\x\A3A\addons\core\script_component.hpp"
+name = QUOTE(Antistasi - Community VERSION);
+author = "Antistasi Dev Team";
+logo = "antistasi_logo_sw.paa";
+logoOver = "antistasi_logo.paa";
+logoSmall = "antistasi_logo_small.paa";
+picture = "antistasi_overview.paa";
 description = "";
 tooltip = "Antistasi";
-overview = "";
+tooltipOwned = "Antistasi";
+overview = "<br/><t align='center' size='0.75'>Visit Antistasi on our <t href='https://antistasi.de/'>Website</t> | <t href='http://antistasi.de/discord'>Discord</t> | <t href='https://twitter.com/antistasio'>Twitter</t></t><br/><br/>";
 actionName = "GitHub";
-action = "https://github.com/";
+action = "https://github.com/official-antistasi-community/A3-Antistasi";
+hideName = 0;
+hidePicture = 0;
+dlcColor[] = {0.1765, 0.6392, 0.6235, 1};
