7d88a8552e9d7a7f1d6c7e7d7462b0846966af38
Håkon <hrr1@hotmail.no>
2022-10-19 20:08:08 +0200

    added a dev mod.cpp for dev builds to be distinguised in the launcher/menu

---
+++ A3A/mod_dev.cpp
@@ -0,0 +1,20 @@
+#if __has_include("\x\A3A\addons\core\script_component.hpp")
+#include "\x\A3A\addons\core\script_component.hpp"
+name = QUOTE([Dev] Antistasi - Community VERSION);
+logo = QPATHTOFOLDER(Pictures\antistasi_logo_sw.paa);
+logoOver = QPATHTOFOLDER(Pictures\antistasi_logo.paa);
+logoSmall = QPATHTOFOLDER(Pictures\antistasi_logo_small.paa);
+picture = QPATHTOFOLDER(Pictures\antistasi_overview.paa);
+#else
+name = "[Dev] Antistasi - Community version";
+#endif
+author = "Antistasi Dev Team";
+description = "";
+tooltip = "[Dev] Antistasi";
+tooltipOwned = "Antistasi";
+overview = "<br/><t align='center' size='0.75'>Visit Antistasi on our <t href='https://antistasi.de/'>Website</t> | <t href='http://antistasi.de/discord'>Discord</t> | <t href='https://twitter.com/antistasio'>Twitter</t></t><br/><br/>";
+actionName = "GitHub";
+action = "https://github.com/official-antistasi-community/A3-Antistasi";
+hideName = 0;
+hidePicture = 0;
+dlcColor[] = {0.1765, 0.6392, 0.6235, 1};
