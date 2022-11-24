b17a49ec74106066e6e29aef93c691cfc1bbba8d
HakonRydland <61709767+HakonRydland@users.noreply.github.com>
2022-11-24 20:04:48 +0100

    removed old logistics and accompanying nodes (#2560)
    * removed old logistics and accompanying nodes
    
    * added missing entries for canLoadWeapon = 0

--- A3A/addons/core/CfgFunctions.hpp
+++ A3A/addons/core/CfgFunctions.hpp
@@ -385,35 +385,6 @@ class CfgFunctions
             class keyActions {};
         };
 
-        class Logistics {
-            file = QPATHTOFOLDER(functions\Logistics);
-            class logistics_addLoadAction {};
-            class logistics_getVehCapacity {};
-            class logistics_initNodes {};
-            class logistics_isLoadable {};
-        };
-
-        class LogisticsFunctions {
-            file = QPATHTOFOLDER(functions\Logistics\functions);
-            class logistics_addAction {};
-            class logistics_addOrRemoveObjectMass {};
-            class logistics_addWeaponAction {};
-            class logistics_attachCargo {};
-            class logistics_canLoad {};
-            class logistics_generateHardPoints {};
-            class logistics_getCargoNodeType {};
-            class logistics_getCargoOffsetAndDir {};
-            class logistics_getVehicleNodes {};
-            class logistics_initMountedWeapon {};
-            class logistics_load {};
-            class logistics_refreshVehicleLoad {};
-            class logistics_removeWeaponAction {};
-            class logistics_toggleAceActions {};
-            class logistics_toggleLock {};
-            class logistics_tryLoad {};
-            class logistics_unload {};
-        };
-
         class LTC {
             file = QPATHTOFOLDER(functions\LTC);
             class canLoot {};
