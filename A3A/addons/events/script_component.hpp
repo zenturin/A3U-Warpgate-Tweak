9be5d4274734a61cb134b42df31d99b787bb970a
HakonRydland <61709767+HakonRydland@users.noreply.github.com>
2023-01-22 13:28:25 +0100

    Lower case all (#2633)
    * lower cased addons, their pboprefix, and component
    
    * lowercase folders

--- A3A/addons/events/script_component.hpp
+++ A3A/addons/events/script_component.hpp
@@ -1,2 +1,2 @@
-#define COMPONENT GUI
+#define COMPONENT events
 #include "\x\A3A\addons\core\Includes\script_mod.hpp"
--- A3A/addons/gui/script_component.hpp
+++ A3A/addons/gui/script_component.hpp
@@ -1,2 +1,2 @@
-#define COMPONENT Garage
+#define COMPONENT gui
 #include "\x\A3A\addons\core\Includes\script_mod.hpp"
