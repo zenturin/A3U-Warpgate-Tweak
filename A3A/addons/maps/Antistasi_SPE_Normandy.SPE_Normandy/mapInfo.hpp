cb9a070c03d522e93fbdb9673a929f0f170fdae9
Bob-Murphy <Bob-Murphy@users.noreply.github.com>
2023-02-28 00:47:33 +0100

    Normandy-first-integration

---
+++ A3A/addons/maps/Antistasi_SPE_Normandy.SPE_Normandy/mapInfo.hpp
@@ -0,0 +1,13 @@
+class SPE_Normandy {
+	population[] = {}; //automated data is fine and needs no adaptation
+	disabledTowns[] = {};
+	antennas[] = {
+	};
+	antennasBlacklistIndex[] = {};
+	banks[] = {}; //only one possible building but directly next to a factory
+	garrison[] = {{},{},{},{}};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F"
+	};
+	climate = "arid";
+};
