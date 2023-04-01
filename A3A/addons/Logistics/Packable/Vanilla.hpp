32ec7d9df400a11b2ea2c994174132138991b678
Bob-Murphy <50651578+Bob-Murphy@users.noreply.github.com>
2023-04-01 19:33:02 +0200

    Delete A3A/addons/Logistics/Packable directory
    removing dublicate

--- A3A/addons/Logistics/Packable/Vanilla.hpp
+++
@@ -1,9 +0,0 @@
-class Land_MedicalTent_01_MTP_closed_F: TRIPLES(ADDON,Packable,Base)
-{
-	packObject = "CargoNet_01_box_F";
-};
-
-class Land_RepairDepot_01_green_F: TRIPLES(ADDON,Packable,Base)
-{
-	packObject = "Land_Cargo10_military_green_F";
-};
