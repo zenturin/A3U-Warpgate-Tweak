7214972bb8ff187a7d94d3c335dbb47705f997b8
MadBender <ryder86@gmail.com>
2023-07-06 02:07:17 +0300

    Fix some RHS flags (#2790)
    * Update publishBranchToSteam.yml
    
    * fix flags for CDF, NAPA and SAF
    
    ---------
    
    Co-authored-by: Bob-Murphy <50651578+Bob-Murphy@users.noreply.github.com>
    Co-authored-by: Artyom Vasilyev <vasilyev.artem.v@gmail.com>

---
+++ A3A/addons/core/CfgMarkers.hpp
@@ -0,0 +1,21 @@
+class CfgMarkers {
+	class flag_NATO;
+
+	class a3a_flag_cdf: flag_NATO {
+		name = "CDF";
+		icon = QPATHTOFOLDER(data\map\markers\flags\cdf_ca.paa);
+		texture = QPATHTOFOLDER(data\map\markers\flags\cdf_ca.paa);
+	};
+
+	class a3a_flag_napa: flag_NATO {
+		name = "NAPA";
+		icon = QPATHTOFOLDER(data\map\markers\flags\napa_ca.paa);
+		texture = QPATHTOFOLDER(data\map\markers\flags\napa_ca.paa);
+	};
+	
+	class a3a_flag_serbia: flag_NATO {
+		name = "Serbia";
+		icon = QPATHTOFOLDER(data\map\markers\flags\serbia_ca.paa);
+		texture = QPATHTOFOLDER(data\map\markers\flags\serbia_ca.paa);
+	};
+};
