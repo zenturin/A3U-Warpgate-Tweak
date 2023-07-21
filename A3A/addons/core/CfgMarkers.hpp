644c0d460033b61084e1933508460277b1e46a07
ante185 <49555217+ante185@users.noreply.github.com>
2023-07-21 22:50:24 +0200

    RHS Chdkz AI template (#2796)
    * Fix trailing comma and missing AA plane in ION Temperate
    
    * Added ChDZK
    
    marker and marker texture
    
    * Update RHS_AI_ChDKZ.sqf
    
    Added armoured vests, SL vests + backpack, remobed error giving flashlights
    
    * Update RHS_AI_ChDKZ.sqf
    
    SL specific rifles
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    Expanded GL arsenal of militia and military.
    Replaced unique unit definition for SF with single code line604
    
    * Reordered vehicles following feedback
    
    moved rocket hips to LightAttack
    ZU trucks moved to light armed
    Unarmed BRDM2 moved to unarmed, BRDM HQ moved to light armed
    BMDs moved to APC.
    Kraz trucks replaced with Ural fuel truck
    
    * Update RHS_Vehicle_Attributes.sqf
    
    Rocket hip price adjustment removed - only used for light attack now
    
    * Update RHS_Vehicle_Attributes.sqf
    
    Adjusted cost of Mi24P to reflect lesser capabilities
    
    * Testing facewears
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * reduced uniform diversity
    
    too colourful prior
    
    * Update RHS_AI_ChDKZ.sqf
    
    * ekp8_18 incompatible with aks74un,  fixed
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Added ChDZK
    
    marker and marker texture
    
    * Update RHS_AI_ChDKZ.sqf
    
    Added armoured vests, SL vests + backpack, remobed error giving flashlights
    
    * Update RHS_AI_ChDKZ.sqf
    
    SL specific rifles
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    Expanded GL arsenal of militia and military.
    Replaced unique unit definition for SF with single code line604
    
    * Reordered vehicles following feedback
    
    moved rocket hips to LightAttack
    ZU trucks moved to light armed
    Unarmed BRDM2 moved to unarmed, BRDM HQ moved to light armed
    BMDs moved to APC.
    Kraz trucks replaced with Ural fuel truck
    
    * Update RHS_Vehicle_Attributes.sqf
    
    Rocket hip price adjustment removed - only used for light attack now
    
    * Update RHS_Vehicle_Attributes.sqf
    
    Adjusted cost of Mi24P to reflect lesser capabilities
    
    * Testing facewears
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * reduced uniform diversity
    
    too colourful prior
    
    * Update RHS_AI_ChDKZ.sqf
    
    * ekp8_18 incompatible with aks74un,  fixed
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Update RHS_AI_ChDKZ.sqf
    
    * Moved flag textures
    
    moved flag textures of #2790 to location of #2796
    
    ---------
    
    Co-authored-by: John Jordan <jaj22x@gmail.com>
    Co-authored-by: Lazejun <57111907+Lazejun@users.noreply.github.com>

--- A3A/addons/core/CfgMarkers.hpp
+++ A3A/addons/core/CfgMarkers.hpp
@@ -3,19 +3,24 @@ class CfgMarkers {
 
 	class a3a_flag_cdf: flag_NATO {
 		name = "CDF";
-		icon = QPATHTOFOLDER(data\map\markers\flags\cdf_ca.paa);
-		texture = QPATHTOFOLDER(data\map\markers\flags\cdf_ca.paa);
+		icon = QPATHTOFOLDER(Pictures\Markers\cdf_ca.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\cdf_ca.paa);
 	};
 
 	class a3a_flag_napa: flag_NATO {
 		name = "NAPA";
-		icon = QPATHTOFOLDER(data\map\markers\flags\napa_ca.paa);
-		texture = QPATHTOFOLDER(data\map\markers\flags\napa_ca.paa);
+		icon = QPATHTOFOLDER(Pictures\Markers\napa_ca.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\napa_ca.paa);
 	};
 	
 	class a3a_flag_serbia: flag_NATO {
 		name = "Serbia";
-		icon = QPATHTOFOLDER(data\map\markers\flags\serbia_ca.paa);
-		texture = QPATHTOFOLDER(data\map\markers\flags\serbia_ca.paa);
+		icon = QPATHTOFOLDER(Pictures\Markers\serbia_ca.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\serbia_ca.paa);
+	};
+	class a3a_flag_chdkz: flag_NATO {
+		name = "ChDKZ";
+		icon = QPATHTOFOLDER(Pictures\Markers\chdkz_co.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\chdkz_co.paa);
 	};
 };
