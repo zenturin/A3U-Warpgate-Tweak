2c7efa77a1b176dee334be34c02673533097fba9
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2023-08-08 22:26:28 +0100

    Update mod.cpp

--- A3A/mod.cpp
+++ A3A/mod.cpp
@@ -1,20 +1,20 @@
 #if __has_include("\x\A3A\addons\core\script_component.hpp")
 #include "\x\A3A\addons\core\script_component.hpp"
-name = QUOTE(Antistasi Plus);
+name = QUOTE(Antistasi Ultimate);
 logo = QPATHTOFOLDER(Pictures\antistasi_plus_logo_sw.paa);
 logoOver = QPATHTOFOLDER(Pictures\antistasi_plus_logo.paa);
 logoSmall = QPATHTOFOLDER(Pictures\antistasi_plus_logo_small.paa);
 picture = QPATHTOFOLDER(Pictures\antistasi_plus_overview.paa);
 #else
-name = "Antistasi Plus";
+name = "Antistasi Ultimate";
 #endif
-author = "Socrates";
+author = "Antistasi Ultimate Dev Team";
 description = "";
-tooltip = "Antistasi Plus";
-tooltipOwned = "Antistasi Plus";
-overview = "<br/><t align='center' size='0.75'> An Arma 3 persistent multiplayer scenario about guerilla warfare tactics. Fork of Antistasi Community Version focused on expanding player's capabilities and introducing new gameplay features. </t><br/><br/>";
+tooltip = "Antistasi Ultimate";
+tooltipOwned = "Antistasi Ultimate";
+overview = "<br/><t align='center' size='0.75'> An Arma 3 persistent multiplayer scenario about guerilla warfare tactics. Fork of Socrate's Antistasi Plus, which is a fork of the Antistasi Community Version. We focused on expanding playable mods, factions and maps. </t><br/><br/>";
 actionName = "GitHub";
-action = "https://github.com/igorkis-scrts/A3-Antistasi-Plus";
+action = "https://github.com/SilenceIsFatto/A3-Antistasi-Ultimate";
 hideName = 0;
 hidePicture = 0;
 dlcColor[] = {0.1765, 0.6392, 0.6235, 1};
