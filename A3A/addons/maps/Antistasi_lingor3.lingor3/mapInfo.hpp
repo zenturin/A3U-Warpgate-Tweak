35733589920730c3e7f32045fb867b92ee068d4e
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2023-08-08 12:37:18 +0100

    Lingor Map Port
    Population, disabled towns, antennas, mil admin buildings, navgrid
    all completed

---
+++ A3A/addons/maps/Antistasi_lingor3.lingor3/mapInfo.hpp
@@ -0,0 +1,19 @@
+class lingor3 {
+	population[] = {
+		{"tucos",200},{"maruko",903},{"calamar",549},{"montehofo",29},{"manteria",125},{"sanvigado",54},{"guayucca",153},{"calapedro",87},{"tia",82},{"mercielo",93},{"calixo",123},{"fernando",428},{"aculto",72},{"garibosa",225},{"haciendos",96},{"mairango",76},{"rosalia",112},{"elcanto",85},{"morada",155},{"victorin",66},{"prospero",57},{"corazon",260},{"fauna",124},{"negrosa",28},{"peligron",35},{"sanisabel",94},{"marcella",44},{"pancho",32},{"lagosa",17},{"aguado",28},{"barro",48},{"gomez",21},{"conoteta",35},{"cemarin",14},{"madron",31},{"picolin",12},{"pmalargo",0},{"villon",156},{"palida",64},{"chupinka",10},{"tamba",51},{"pachi",37},{"cabara",45},{"monga",180},{"ayan",15},{"pikawas",32},{"bilbado",148},{"ralon",82},{"sanarulco",571},{"benio",283},{"pocobay",36},{"reginatributa",67},{"pintosa",254},{"drassen",261},{"rago",80},{"medlina",120},{"zuela",233},{"alma",22},{"cordo",23},{"mercadio",41},{"verto",54},{"zanjeer",75},{"gatoro",69},{"medicolin",51},{"arapesca",49},{"fantasmo",170},{"maruko",903},{"matacan",105},{"vidora",155},{"paredes",29},{"mvt",25},{"mvv",13},{"mirasol",94},{"jodido",13},{"pescito",34},{"ramona",75},{"gritto",124},{"vissa",35}
+	};
+	disabledTowns[] = {"montehofo","sanvigado","guayucca","calapedro","tia","mercielo","calixo","aculto","haciendos","mairango", "rosalia","elcanto","negrosa","marcella","pancho","aguado","picolin","chupinka","tamba","pachi","cabara","ayan","ralon","pocobay","drassen","rago","alma","cordo","mercadio","mvt","mvv","pescito"};
+	antennas[] = {
+		{1817.68,497.62,0},{1229.9,790.104,0},{1910.62,1194.67,0},{3112.08,1492.05,0},{3873.38,1732.36,0},{4464.64,2602.47,0},{1253.68,2668.2,0},{538.699,3192.1,0},{2629.44,9818.01,0},{1741.13,3463.85,0},{8323.76,1223.77,0},{6105.02,3448.71,0},{6304.57,3954.24,0},{6749.56,4442.81,0},{6045.61,5674.38,0},{6197.25,6274.53,0},{6003.96,6722.71,0},{6317.5,6846.85,0},{4154.08,7615.34,0},{8815.87,8023.35,0},{9229.89,5043.07,0},{5707.88,4117.37,0},{5456.02,5563.94,0},{4234.43,4228.4,0},{3497.99,3738.21,0},{507.001,9934.24,0},{1002.589,3137.505,0},{2857.373,3549.940,0},{6116.803,1660.465,0},{4377.535,6760.375,0},{4135.021,1642.403,0},{9253.652,8226.361,0},{5679.58,9555.188,0},{2669.333,6757.543,0}
+	};
+	antennasBlacklistIndex[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,21,22,23,24};
+	banks[] = {}; //no suitable building available
+	garrison[] = {
+		{},{"airport_3", "milbase_1","resource_1", "outpost_6", "outpost_10", "outpost_11", "outpost_12", "outpost_16", "outpost_18", "control_25", "control_29", "control_24", "control_30", "control_19", "control_21", "control_22", "control_20", "control_23", "control_25", "control_29", "control_63", "control_62", "control_24", "control_64", "control_30", "control_19", "control_21", "control_22", "control_20", "control_23"},{},{"control_25", "control_29", "control_24", "control_30", "control_19", "control_21", "control_22", "control_20", "control_23", "control_25", "control_29", "control_63", "control_62", "control_24", "control_64", "control_30", "control_19", "control_21", "control_22", "control_20", "control_23"}
+	};
+	fuelStationTypes[] = {"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
+	milAdministrations[] = {
+		{6386.377,5943.387,0},{984.796,5828.121,0},{4218.354,2437.375,0},{815.002,8200.398,0},{3310.242,9738.308,0}
+	};
+	climate = "tropical";
+};
