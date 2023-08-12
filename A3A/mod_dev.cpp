5d9ea9a6f6c44d93802b81c1f2fe3eb1ed4c4728
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2023-08-12 16:17:59 +0100

    Update mod_dev.cpp

--- A3A/mod_dev.cpp
+++ A3A/mod_dev.cpp
@@ -14,7 +14,7 @@ tooltip = "[Dev] Antistasi Plus";
 tooltipOwned = "Antistasi Plus";
 overview = "<br/><t align='center' size='0.75'> An Arma 3 persistent multiplayer scenario about guerilla warfare tactics. Fork of Antistasi Community Version focused on expanding player's capabilities and introducing new gameplay features. </t><br/><br/>";
 actionName = "GitHub";
-action = "https://github.com/igorkis-scrts/A3-Antistasi-Plus";
+action = "https://github.com/SilenceIsFatto/A3-Antistasi-Ultimate";
 hideName = 0;
 hidePicture = 0;
 dlcColor[] = {0.1765, 0.6392, 0.6235, 1};
