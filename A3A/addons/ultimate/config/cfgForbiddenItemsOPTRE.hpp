b2f3f70c38a02cea6a1b2b7a4a58db03b2b5d27f
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2023-08-13 17:25:17 +0100

    fix double entry

--- A3A/addons/ultimate/config/cfgForbiddenItemsOPTRE.hpp
+++ A3A/addons/ultimate/config/cfgForbiddenItemsOPTRE.hpp
@@ -29,7 +29,6 @@
     class OPTRE_FC_Elite_Helmet_SpecOps : forbidden_limited_base {};
     class OPTRE_FC_Elite_Helmet_Ultra : forbidden_limited_base {};
     class OPTRE_FC_Elite_Helmet_Zealot : forbidden_limited_base {};
-    class OPTRE_FC_Elite_Helmet_Zealot : forbidden_limited_base {};
 
     class OPTRE_FC_T33_FuelRod_Cannon_Guided : forbidden_limited_base {};
     class OPTRE_FC_T33_FuelRod_Cannon : forbidden_limited_base {};
