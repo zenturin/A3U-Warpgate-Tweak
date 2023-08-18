b0fe31e7494f27900ce66a3514c0e85fe69c78fb
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2023-08-18 20:59:00 +0100

    add fwa magazines

--- A3A/addons/hals/Addons/store/config/fwa.hpp
+++ A3A/addons/hals/Addons/store/config/fwa.hpp
@@ -23,7 +23,7 @@
 			ITEM(sp_fwa_enfield_l42_beech, 600, RIFLE_STOCK); // sp_fwa_10Rnd_762_L42
 			ITEM(sp_fwa_enfield_l42_walnut, 600, RIFLE_STOCK);
 
-			ITEM(sp_fwa_enfield_l8, 600, RIFLE_STOCK); // sp_fwa_10Rnd_762_L42
+			ITEM(sp_fwa_enfield_l8, 600, RIFLE_STOCK);
 			ITEM(sp_fwa_enfield_l8_walnut, 600, RIFLE_STOCK);
 
 			ITEM(sp_fwa_enfield_no5, 600, RIFLE_STOCK); // sp_fwa_10Rnd_303_No4
@@ -145,78 +145,95 @@
 			///////////////////////////////////////////////////////
 			// Pistols, SMGs
 			///////////////////////////////////////////////////////
-			class hlc_30Rnd_9x19_B_MP5 {
+			class sp_fwa_36Rnd_9mm_carlg_m45 {
+				price = 50;
+				stock = MAGAZINE_STOCK;
+			};
+			class sp_fwa_15Rnd_9x19_L2A3_Sterling {
+				price = 25;
+				stock = MAGAZINE_STOCK;
+			};
+			class sp_fwa_30Rnd_45acp_m3a1 {
 				price = 50;
 				stock = MAGAZINE_STOCK;
 			};
 			///////////////////////////////////////////////////////
 			// RIFLES
 			///////////////////////////////////////////////////////
-			class hlc_30Rnd_545x39_B_AK {
+			class sp_fwa_20Rnd_762_ar10 {
 				price = 100;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_30Rnd_762x39_b_ak {
+			class sp_fwa_20rnd_762_bm59 {
 				price = 110;
 				stock = MAGAZINE_STOCK;
 			};
 
-			class hlc_60Rnd_545x39_b_rpk {
+			class sp_fwa_6Rnd_65carcano_m38 {
 				price = 250;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_50rnd_300BLK_STANAG_EPR {
-				price = 200;
+			class sp_fwa_20Rnd_556_Armalite_Waffle {
+				price = 50;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_20Rnd_762x51_B_fal {
+			class sp_fwa_30Rnd_556_Armalite_Ball {
 				price = 75;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_20Rnd_762x51_B_M14 {
-				price = 75;
+			class sp_fwa_20Rnd_762_FAL_Metric {
+				price = 90;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_30rnd_556x45_EPR {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30rnd_556x45_TDim_L5 {
-				price = 65;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30rnd_556x45_EPR_PMAG {
-				price = 65;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_20Rnd_762x51_B_SCARH_Tan {
+			class sp_fwa_10Rnd_762_L42 {
 				price = 80;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_30Rnd_556x45_EPR_sg550 {
+			class sp_fwa_10Rnd_303_No4 {
+				price = 80;
+				stock = MAGAZINE_STOCK;
+			};
+			class sp_fwa_10rnd_3006_fn49 {
+				price = 80;
+				stock = MAGAZINE_STOCK;
+			};
+			class sp_fwa_30Rnd_30Carbine_m1 {
+				price = 110;
+				stock = MAGAZINE_STOCK;
+			};
+			class sp_fwa_10rnd_75_mas49 {
 				price = 75;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_30Rnd_556x45_B_AUG {
+			class sp_fwa_5Rnd_792x57_K98 {
+				price = 90;
+				stock = MAGAZINE_STOCK;
+			};
+			class sp_fwa_20Rnd_556_Ruger_Mini14 {
 				price = 75;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_25Rnd_9x19mm_M882_AUG {
+			class sp_fwa_8Rnd_3006_Garand {
 				price = 75;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_30rnd_556x45_EPR_G36 {
-				price = 75;
+			class sp_fwa_30Rnd_792x33_STG44 {
+				price = 90;
+				stock = MAGAZINE_STOCK;
+			};
+			class sp_fwa_32Rnd_9x19_mp40 {
+				price = 90;
 				stock = MAGAZINE_STOCK;
 			};
 
-            // mgs
-			
-			class hlc_50Rnd_792x57_B_MG42 {
+			///////////////////////////////////////////////////////
+			// MG's
+			///////////////////////////////////////////////////////
+			class sp_fwa_50Rnd_3006_mag {
 				price = 225;
 				stock = MAGAZINE_STOCK;
 			};
-			class hlc_100Rnd_762x51_M_M60E4 {
+			class sp_fwa_100Rnd_762_mag {
 				price = 250;
 				stock = MAGAZINE_STOCK;
 			};
@@ -224,297 +241,7 @@
 			///////////////////////////////////////////////////////
 			// DMRs, Sniper Rifles
 			///////////////////////////////////////////////////////
-			class hlc_10Rnd_762x51_B_fal {
-				price = 200;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_5rnd_300WM_FMJ_AWM {
-				price = 300;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_5rnd_3006_1903 {
-				price = 300;
-				stock = MAGAZINE_STOCK;
-			};
-		};		class riflesNIArms
-		{
-			displayName = "NIArms Rifles";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\primaryWeapon_ca.paa";
-
-			ITEM(hlc_rifle_akm, 250, RIFLE_STOCK);  // hlc_30Rnd_762x39_b_ak
-			ITEM(hlc_rifle_ak47, 350, RIFLE_STOCK);
-			ITEM(hlc_rifle_ak74, 400, RIFLE_STOCK); // hlc_30Rnd_545x39_B_AK
-			ITEM(hlc_rifle_ak74m, 450, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_ak12, 700, RIFLE_STOCK);
-			ITEM(hlc_rifle_aku12, 550, RIFLE_STOCK);
-			ITEM(hlc_rifle_ak12GL, 850, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_RPK12, 600, RIFLE_STOCK); // hlc_60Rnd_545x39_b_rpk
-            
-			ITEM(hlc_rifle_SLR, 500, RIFLE_STOCK); // hlc_20Rnd_762x51_B_fal
-			ITEM(hlc_rifle_M14, 550, RIFLE_STOCK); // hlc_20Rnd_762x51_B_M14
-			ITEM(hlc_rifle_M14dmr_Rail, 750, RIFLE_STOCK);
-
-            // blacklist: HLC_wp_M134Painless
-
-			ITEM(hlc_wp_xm4, 800, RIFLE_STOCK); // hlc_30rnd_556x45_EPR
-			ITEM(hlc_wp_XM177E2, 900, RIFLE_STOCK);
-			ITEM(hlc_rifle_mk18mod0, 900, RIFLE_STOCK); // hlc_30rnd_556x45_TDim_L5
-			ITEM(hlc_wp_MK17IUR_556, 950, RIFLE_STOCK); // hlc_30rnd_556x45_EPR_PMAG
-			ITEM(hlc_WP_MK17IUR, 1150, RIFLE_STOCK); // hlc_20Rnd_762x51_B_SCARH_Tan
-
-			ITEM(hlc_rifle_ACR_carb_black, 800, RIFLE_STOCK);
-			ITEM(hlc_rifle_ACR_Full_black, 950, RIFLE_STOCK);
-			ITEM(hlc_rifle_samr2, 1000, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_SG550, 800, RIFLE_STOCK); // hlc_30Rnd_556x45_EPR_sg550
-			ITEM(hlc_rifle_SG550_TAC, 950, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_augsrcarb_b, 800, RIFLE_STOCK); // hlc_30Rnd_556x45_B_AUG
-			ITEM(hlc_rifle_augsrhbar_b, 950, RIFLE_STOCK);
-			ITEM(hlc_rifle_auga1_b, 900, RIFLE_STOCK);
-			ITEM(hlc_rifle_auga1carb_B, 900, RIFLE_STOCK);
-			ITEM(hlc_rifle_augpara_b, 800, RIFLE_STOCK); // hlc_25Rnd_9x19mm_M882_AUG
-
-			ITEM(hlc_rifle_auga3_b, 1000, RIFLE_STOCK);
-			ITEM(hlc_rifle_auga3, 1000, RIFLE_STOCK);
-
-			ITEM(HLC_rifle_XM8_Carbine, 1250, RIFLE_STOCK); // hlc_30rnd_556x45_EPR_G36
-			ITEM(HLC_Rifle_XM8_Proto_Carbine, 1200, RIFLE_STOCK);
-			ITEM(HLC_rifle_XM8_Carbine_GL, 1400, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_honeybadger, 1250, RIFLE_STOCK); // hlc_50rnd_300BLK_STANAG_EPR
-			ITEM(hlc_rifle_RU5562, 1200, RIFLE_STOCK);
-			ITEM(hlc_rifle_bcmblackjack, 1400, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_M4a1carryhandle, 1250, RIFLE_STOCK);
-			ITEM(hlc_rifle_CQBR, 1200, RIFLE_STOCK);
-			ITEM(hlc_rifle_Colt727, 1400, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_G36C, 2150, RIFLE_STOCK); // hlc_30rnd_556x45_EPR_G36
-			ITEM(hlc_rifle_G36CV, 2250, RIFLE_STOCK);
-			ITEM(hlc_rifle_G36KV, 2350, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_416D10C, 1200, RIFLE_STOCK);
-			ITEM(hlc_rifle_416D10_st6, 1400, RIFLE_STOCK);
-			ITEM(hlc_rifle_416D145_CAG, 1500, RIFLE_STOCK);
-		};
-
-		class sniperRiflesNIArms 
-		{
-			displayName = "NIArms Snipers";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\primaryWeapon_ca.paa";
-
-			ITEM(hlc_rifle_M1903A1_unertl, 1000, RIFLE_STOCK); // hlc_5rnd_3006_1903
-			ITEM(hlc_rifle_FN3011, 1450, RIFLE_STOCK); // hlc_10Rnd_762x51_B_fal
-			ITEM(hlc_rifle_FN3011Modern, 1450, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_awcovert_BL, 1600, RIFLE_STOCK); // hlc_5rnd_300WM_FMJ_AWM
-			ITEM(hlc_rifle_awcovert_FDE, 1600, RIFLE_STOCK);
-			ITEM(hlc_rifle_awcovert, 1600, RIFLE_STOCK);
-
-			ITEM(hlc_rifle_awmagnum_BL, 1500, RIFLE_STOCK);
-			ITEM(hlc_rifle_awmagnum_FDE, 1500, RIFLE_STOCK);
-			ITEM(hlc_rifle_awmagnum, 1500, RIFLE_STOCK);
-			ITEM(hlc_rifle_SG550Sniper_RIS, 1500, RIFLE_STOCK);
-		};
-
-		class mgNIArms
-		{
-			displayName = "NIArms MGs";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\primaryWeapon_ca.paa";
-
-			ITEM(hlc_lmg_MG42, 1500, RIFLE_STOCK); // hlc_50Rnd_792x57_B_MG42
-			ITEM(hlc_lmg_mg42kws_b, 1500, RIFLE_STOCK);
-			ITEM(hlc_lmg_MG3_optic, 1500, RIFLE_STOCK);
-
-			ITEM(hlc_lmg_mk48mod1, 1600, RIFLE_STOCK); // hlc_100Rnd_762x51_M_M60E4
-			ITEM(hlc_lmg_mk48, 1600, RIFLE_STOCK);
-
-			ITEM(hlc_lmg_M60E4, 1600, RIFLE_STOCK);
-			ITEM(hlc_lmg_m60, 1600, RIFLE_STOCK);
-		};
-
-		class smgNIArms
-		{
-			displayName = "NIArms SMGs";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\primaryWeapon_ca.paa";
-
-			ITEM(hlc_smg_mp5a2, 350, RIFLE_STOCK); // hlc_30Rnd_9x19_B_MP5
-			ITEM(hlc_smg_mp5a2_tac, 350, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5a3, 350, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5a3_tac, 350, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5a4, 350, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5a4_tac, 350, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5k_PDW, 350, RIFLE_STOCK);
-
-			ITEM(hlc_smg_MP5N, 450, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5N_tac, 450, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5sd5, 450, RIFLE_STOCK);
-			ITEM(hlc_smg_mp5sd6, 450, RIFLE_STOCK);
-		};
-
-		class underbarrelNIArms
-		{
-			displayName = "NIArms Bipods";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemBipod_ca.paa";
-
-			ITEM(hlc_acc_vfg_pcap, 100, 50);
-			ITEM(hlc_grip_pmvfg_black, 100, 50);
-			ITEM(hlc_grip_afg2, 100, 50);
-			ITEM(hlc_bipod_utgshooters, 100, 50);
-		};
-
-		class pointersNIArms 
-		{
-			displayName = "NIArms Pointers";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemAcc_ca.paa";
-
-			ITEM(hlc_panel_side_mp5tac_magpulxt_black, 75, PN_STOCK);
-		};
-
-		class muzzlesNIArms 
-		{
-			displayName = "NIArms Muzzles";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemMuzzle_ca.paa";
-
-			ITEM(hlc_muzzle_agendasix, 500, MZ_STOCK); //5
-			ITEM(hlc_muzzle_556nato_m42000, 500, MZ_STOCK);
-			ITEM(hlc_muzzle_snds_rotex3p, 500, MZ_STOCK);
-		};
-
-		class opticsNIArms
-		{
-			displayName = "NIArms Optics";
-			picture = "";
-
-			class hlc_optic_zf95base {
-				price = 1500;
-				stock = 100;
-			};
-			class hlc_optic_leupoldm3a {
-				price = 1500;
-				stock = 100;
-			};
-			class hlc_optic_hensoldtzo_lo {
-				price = 700;
-				stock = 100;
-			};
-			class hlc_optic_hensoldtzo_lo_docter {
-				price = 800;
-				stock = 100;
-			};
-			class hlc_optic_hensoldtzo_hi {
-				price = 700;
-				stock = 100;
-			};
-			class hlc_optic_hensoldtzo_hi_2d {
-				price = 800;
-				stock = 100;
-			};
-			class hlc_optic_ism1400a7 {
-				price = 650;
-				stock = 100;
-			};
-		};
-
-		class magazinesNIArms 
-		{
-			displayName = "NIArms Magazines";
-			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\cargoMag_ca.paa";
-            
-			///////////////////////////////////////////////////////
-			// Pistols, SMGs
-			///////////////////////////////////////////////////////
-			class hlc_30Rnd_9x19_B_MP5 {
-				price = 50;
-				stock = MAGAZINE_STOCK;
-			};
-			///////////////////////////////////////////////////////
-			// RIFLES
-			///////////////////////////////////////////////////////
-			class hlc_30Rnd_545x39_B_AK {
-				price = 100;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30Rnd_762x39_b_ak {
-				price = 110;
-				stock = MAGAZINE_STOCK;
-			};
-
-			class hlc_60Rnd_545x39_b_rpk {
-				price = 250;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_50rnd_300BLK_STANAG_EPR {
-				price = 200;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_20Rnd_762x51_B_fal {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_20Rnd_762x51_B_M14 {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30rnd_556x45_EPR {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30rnd_556x45_TDim_L5 {
-				price = 65;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30rnd_556x45_EPR_PMAG {
-				price = 65;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_20Rnd_762x51_B_SCARH_Tan {
-				price = 80;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30Rnd_556x45_EPR_sg550 {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30Rnd_556x45_B_AUG {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_25Rnd_9x19mm_M882_AUG {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_30rnd_556x45_EPR_G36 {
-				price = 75;
-				stock = MAGAZINE_STOCK;
-			};
-
-            // mgs
-			
-			class hlc_50Rnd_792x57_B_MG42 {
-				price = 225;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_100Rnd_762x51_M_M60E4 {
-				price = 250;
-				stock = MAGAZINE_STOCK;
-			};
-
-			///////////////////////////////////////////////////////
-			// DMRs, Sniper Rifles
-			///////////////////////////////////////////////////////
-			class hlc_10Rnd_762x51_B_fal {
-				price = 200;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_5rnd_300WM_FMJ_AWM {
-				price = 300;
-				stock = MAGAZINE_STOCK;
-			};
-			class hlc_5rnd_3006_1903 {
+			class sp_fwa_20Rnd_762_FN49 {
 				price = 300;
 				stock = MAGAZINE_STOCK;
 			};
