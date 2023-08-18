be2b37ec953da34f4590d9280461fe0b886c029c
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2023-08-18 12:37:06 +0100

    CSA38 Templates

---
+++ A3A/addons/logistics/Nodes/CSA.hpp
@@ -0,0 +1,46 @@
+class CSA38_pragaRV4 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.31,-0.591};
+			seats[] = {1,2,7,10,11,15};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.11,-0.591};
+			seats[] = {3,4,5,6,14};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.91,-0.591};
+			seats[] = {8,9,12,13};
+        };
+    };
+};
+
+class CSA38_pragaRV : TRIPLES(ADDON,Nodes,Base)
+{
+		canLoadWeapon = 0;
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.31,-0.591};
+			seats[] = {1,2,7,10,11,15};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.11,-0.591};
+			seats[] = {3,4,5,6,14};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.91,-0.591};
+			seats[] = {8,9,12,13};
+        };
+    };
+};
+
+class CSA38_pragaRV6 : CSA38_pragaRV {};
