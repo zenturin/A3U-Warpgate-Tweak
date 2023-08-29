9ca264f98c8934508d80a2769437e5934beb8866
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2023-08-29 22:17:21 +0100

    IFA3 Tobruk Map

---
+++ A3A/addons/maps/Antistasi_iron_excelsior_Tobruk.iron_excelsior_Tobruk/mapInfo.hpp
@@ -0,0 +1,19 @@
+class iron_excelsior_Tobruk {
+	population[] = {
+		{"baccara", 110},{"marcuca", 59},{"solaro", 13},{"hariga", 122},{"perrone", 0},{"hotel", 143},{"tobruk", 1039},{"birauda", 151},{"hospital", 51},{"airenti", 3},{"oupost2", 499},{"kingscross", 25},{"outpost1", 0},{"maclub", 131},{"gubbi", 119},{"mahmud", 125},{"marsatobruk", 112},{"daud", 147},{"distillery", 14}
+	};
+	disabledTowns[] = {"marcuca","solaro","perrone","hospital","airenti","kingscross","outpost1","distillery"};
+	antennas[] = {
+	{4948.738,1732.944,0},{475.454,7961.95,0},{8831.038,1038.956,0},{5276.479,6441.453,0}
+};
+	antennasBlacklistIndex[] = {};
+	banks[] = {}; //no suitable building available
+	garrison[] = {
+		{},{"outpost","outpost_1","seaport","seaport_1","control"},{},{"control"}
+	};
+	fuelStationTypes[] = {"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
+	milAdministrations[] = {
+		{5997.239,6149.627,0.197}
+	};
+	climate = "arid";
+};
--- A3A/addons/maps/NavGrid.hpp
+++ A3A/addons/maps/NavGrid.hpp
@@ -30,6 +30,7 @@ class NavGrid {
 	OPTRE_Madrigal = QPATHTOFOLDER(Antistasi_OPTRE_Madrigal.OPTRE_Madrigal\navGrid.sqf);
 	rhspkl = QPATHTOFOLDER(Antistasi_rhspkl.rhspkl\navGrid.sqf);
 	Kapaulio = QPATHTOFOLDER(Antistasi_Kapaulio.Kapaulio\navGrid.sqf);
+	iron_excelsior_Tobruk = QPATHTOFOLDER(Antistasi_iron_excelsior_Tobruk.iron_excelsior_Tobruk\navGrid.sqf);
 	gm_weferlingen_winter = QPATHTOFOLDER(Antistasi_gm_weferlingen_winter.gm_weferlingen_winter\navGrid.sqf);
     gm_weferlingen_summer = QPATHTOFOLDER(Antistasi_gm_weferlingen_summer.gm_weferlingen_summer\navGrid.sqf);
 };
