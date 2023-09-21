8f2479773d00c651ad44b0ba8549985086c8e4f8
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2023-09-21 13:47:18 +0100

    actually fix indentation

--- A3A/addons/hals/Addons/store/config.hpp
+++ A3A/addons/hals/Addons/store/config.hpp
@@ -545,44 +545,45 @@ class cfgHALsStore
 				"launcherscsa"
 			};
 		};
-	class scion
-	{
-		displayName = $STR_ARMS_DEALER_STORE;
-		categories[] = {
-			"riflesScion", 
-			"magazinesriflesScion",
-			"sniperRiflesScion", 
-			"magazinessniperRiflesScion", 
-			"opticsScion"
+		class scion
+		{
+			displayName = $STR_ARMS_DEALER_STORE;
+			categories[] = {
+				"riflesScion", 
+				"magazinesriflesScion",
+				"sniperRiflesScion", 
+				"magazinessniperRiflesScion", 
+				"opticsScion"
+			};
 		};
-	};
-	class wrs
-	{
-		displayName = $STR_ARMS_DEALER_STORE;
-		categories[] = {
-			"riflesWRS", 
-			"magazinesriflesWRS",
-			"sniperRiflesWRS", 
-			"magazinessniperRiflesWRS"
+		class wrs
+		{
+			displayName = $STR_ARMS_DEALER_STORE;
+			categories[] = {
+				"riflesWRS", 
+				"magazinesriflesWRS",
+				"sniperRiflesWRS", 
+				"magazinessniperRiflesWRS"
+			};
 		};
-	};
-	class brafstock		
-	{
-		displayName = $STR_ARMS_DEALER_STORE;
-		categories[] = {
-			"handgunsBraf", 
-			"launchersBraf", 
-			"riflesBraf", 
-			"sniperRiflesBraf",
-			"specialWeaponsBraf", 
-			"mgBraf", 
-			"smgBraf", 
-			"launcherMagazinesBraf", 
-			"magazinesBraf", 
-			"opticsBraf", 
-			"muzzlesBraf",  
-			"pointersBraf", 
-			"navigationBraf"
+		class brafstock		
+		{
+			displayName = $STR_ARMS_DEALER_STORE;
+			categories[] = {
+				"handgunsBraf", 
+				"launchersBraf", 
+				"riflesBraf", 
+				"sniperRiflesBraf",
+				"specialWeaponsBraf", 
+				"mgBraf", 
+				"smgBraf", 
+				"launcherMagazinesBraf", 
+				"magazinesBraf", 
+				"opticsBraf", 
+				"muzzlesBraf",  
+				"pointersBraf", 
+				"navigationBraf"
+			};
 		};
 	};
 };
