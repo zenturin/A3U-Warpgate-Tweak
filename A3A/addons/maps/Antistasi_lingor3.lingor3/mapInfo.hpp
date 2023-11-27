bf1776f2302ea5824a46cb7860ab6f5406aabd59
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2023-11-27 15:07:42 +0000

    map infos

--- A3A/addons/maps/Antistasi_lingor3.lingor3/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_lingor3.lingor3/mapInfo.hpp
@@ -16,4 +16,14 @@ class lingor3 {
 		{6386.377,5943.387,0},{984.796,5828.121,0},{4218.354,2437.375,0},{815.002,8200.398,0},{3310.242,9738.308,0}
 	};
 	climate = "tropical";
+	buildObjects[] = {
+		// Large vanilla arid structures
+		{"Land_BagBunker_Large_F", 300}, {"Land_BagBunker_Tower_F", 300}, {"Land_BagBunker_Small_F", 60},
+		{"Land_Shed_09_F", 120}, {"Land_Shed_10_F", 140},
+		// Vanilla arid sandbag walls
+		{"Land_BagFence_Long_F", 10}, {"Land_BagFence_Round_F", 10}, {"Land_BagFence_Short_F", 10},
+		// Non-camo vanilla stuff
+		{"Land_SandbagBarricade_01_half_F", 20}, {"Land_SlumWall_01_s_2m_F", 5}, {"Land_PillboxBunker_01_hex_F", 200},
+		{"Land_Barricade_01_4m_F", 30}, {"Land_GuardBox_01_brown_F", 80}, {"Land_Tyres_F", 10}
+	};
 };
