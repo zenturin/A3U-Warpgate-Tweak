15deb85b66ae50619c172e682fcb53ff20e508eb
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2024-01-02 18:56:58 +0000

    faction, logistics

---
+++ A3A/addons/logistics/Nodes/TMT.hpp
@@ -0,0 +1,70 @@
+class TMT_TMT_Air_CH47F_TMT_CH_47F_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    canLoadWeapon = 0;
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,2.6,-2.6};
+        };
+        class Node2
+        {
+            offset[] = {0,1.8,-2.6};
+        };
+        class Node3
+        {
+            offset[] = {0,1,-2.6};
+        };
+        class Node4
+        {
+            offset[] = {0,0.2,-2.6};
+        };
+        class Node5
+        {
+            offset[] = {0,-0.6,-2.6};
+        };
+        class Node6
+        {
+            offset[] = {0,-1.4,-2.6};
+        };
+        class Node7
+        {
+            offset[] = {0,-2.2,-2.6};
+        };
+    };
+};
+
+class TMT_TMT_Air_C130_TMT_C130J_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0.75,2.6,-4.5};
+        };
+        class Node2
+        {
+            offset[] = {0.75,1.8,-4.5};
+        };
+        class Node3
+        {
+            offset[] = {0.75,1,-4.5};
+        };
+        class Node4
+        {
+            offset[] = {0.75,0.2,-4.5};
+        };
+        class Node5
+        {
+            offset[] = {0.75,-0.6,-4.5};
+        };
+        class Node6
+        {
+            offset[] = {0.75,-1.4,-4.5};
+        };
+        class Node7
+        {
+            offset[] = {0.75,-2.2,-4.5};
+        };
+    };
+};
