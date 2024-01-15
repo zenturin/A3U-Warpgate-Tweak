7835183bd0b3132759f4fa5594f120f10b1373bc
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2024-01-15 16:23:48 +0000

    change debug.hpp steamids

--- A3A/addons/maps/MissionDescription/debug.hpp
+++ A3A/addons/maps/MissionDescription/debug.hpp
@@ -1,3 +1,7 @@
 allowFunctionsLog = 1;
-enableDebugConsole[] = {"76561197993477748"};
+enableDebugConsole[] = {
+    "76561198296934215", // Zets/Westalgie
+    "76561198219466935", // SilenceIsFatto
+    "76561198087848420" // Stutpip123
+};
 allowFunctionsRecompile = 1;
