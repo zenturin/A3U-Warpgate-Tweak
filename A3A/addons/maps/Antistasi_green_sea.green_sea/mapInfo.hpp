a0daf5afb1fd1222808297b0fd426ba036734169
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2024-01-26 22:16:00 +0000

    update antennas + mil admins

--- A3A/addons/maps/Antistasi_green_sea.green_sea/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_green_sea.green_sea/mapInfo.hpp
@@ -54,19 +54,16 @@ class green_sea
 	};
 	disabledTowns[] = {"Caribou_isl051","Utes_name"};
 	antennas[] = {
-		{3029.11,2350.27,0.229149},{4547.68,3132.05,0.693176},{3715.81,5984.25,0},{6563.68,3405.56,0.0547333},{5264.35,5314.45,-0.00253296},{6443.78,6545.48,0.0928955},{4967.81,9966.56,0},{8127.52,9151.57,0},{13477.6,3345.84,0.0730896},{13010.1,5964.96,-0.0163116},{12937,12763.6,0.164017},{11241.9,4261.85,-0.0175781},{15694.7,3796.42,0},
-
-		{2834.82,2099.14,0},{17372.9,3274.37,-4.57764e-005},{17724.6,3500.09,0},{18329.7,2555.48,-7.62939e-006},{18830.9,3125.07,0},{18610.1,4617.28,0},{19387.2,5016.62,0},{18293,6820.32,0}
+		{18756.8,3134.26,0},{18289.3,1370.28,0},{15694.5,3797.18,0},{19413,5229.23,0},{4382.49,2113.59,0},{20132.6,13999.8,0},{4783.7,11457.4,0},{12530.4,12106.2,0},{5964.07,12690.3,0},{4490.69,16111.1,0},{12455.4,18906.4,0}
 	};
-	// antennasBlacklistIndex[] = {13,14,15,16,17,18,19,20}; // basically just duplicates of the rest
 	antennasBlacklistIndex[] = {};
 	banks[] = {}; //no suitable building available
 	garrison[] = {
-		{},{"outpost_22","outpost_24","outpost_25","outpost_26","seaport_5","milbase_2","airport_5","resource_10","resource_11","resource_12"},{},{}
+		{},{"outpost_22","outpost_24","outpost_25","outpost_26","seaport_5","milbase_2","airport_5","resource_10","resource_11","resource_12", "resource_13"},{},{}
 	};
 	fuelStationTypes[] = {"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
 	milAdministrations[] = {
-		{10393.766,2419.042,0.291},{11969.223,9217.329,0},{2973.641,5423.228,0}
+		{9914.04,8561.59,0},{11489.5,15359.9,-3.8147e-006},{2493.91,11565.8,0}
 	};
 	climate = "temperate";
 	buildObjects[] = {
