ac0d9217b92c1c244541e1e912af7e09f3fa155e
wersal454 <wroma@bk.ru>
2024-02-27 09:41:34 +0300

    Update Aegis.hpp

--- A3A/addons/logistics/Cargo/Aegis.hpp
+++ A3A/addons/logistics/Cargo/Aegis.hpp
@@ -2,7 +2,7 @@ class A3_Aegis_Armor_F_Aegis_APC_Wheeled_04_APC_Wheeled_04_cannon_F_p3d : TRIPLE
 {
     offset[] = {0,0,1.8};
     rotation[] = {0,1,0};
-    size = 7;
+    size = 8;
 };
 class A3_Aegis_Boat_F_Aegis_Boat_Civil_02_Boat_Civil_02_F_p3d : TRIPLES(ADDON,Cargo,Base)
 {
