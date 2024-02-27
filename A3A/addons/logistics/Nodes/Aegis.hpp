9617ca9f42db525e2a0fe3c4d78f26ed1fe79871
wersal454 <wroma@bk.ru>
2024-02-27 09:33:36 +0300

    Update Aegis.hpp

--- A3A/addons/logistics/Nodes/Aegis.hpp
+++ A3A/addons/logistics/Nodes/Aegis.hpp
@@ -34,3 +34,382 @@ class A3_Aegis_Soft_F_Aegis_Truck_02_Truck_02_flatbed_F_p3d : TRIPLES(ADDON,Node
         };
     };
 };
+class A3_Aegis_Boat_F_Aegis_Boat_Civil_02_Boat_Civil_02_F_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.8,-0.92};
+            seats[] = {2,3};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.6,-0.92};
+            seats[] = {0,1};
+        };
+    };
+};
+class A3_Aegis_Armor_F_Aegis_APC_Wheeled_04_APC_Wheeled_04_cannon_F_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node3
+        {
+            offset[] = {0,-3.33,-0.1};
+        };
+        class Node2
+        {
+            offset[] = {0,-2.53,-0.1};
+        };
+        class Node1
+        {
+            offset[] = {0,-1.73,-0.1};
+        };
+        
+    };
+};
+class A3_Aegis_Air_F_Aegis_Heli_Transport_02_Heli_Transport_02_F_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,3.93,-1.74};
+        };
+        class Node2
+        {
+            offset[] = {0,3.13,-1.74};
+        };
+        class Node3
+        {
+            offset[] = {0,2.33,-1.74};
+        };
+        class Node4
+        {
+            offset[] = {0,1.53,-1.74};
+        };
+        class Node5
+        {
+            offset[] = {0,0.73,-1.74};
+        };
+        class Node6
+        {
+            offset[] = {0,-0.0700001,-1.74};
+        };
+        class Node7
+        {
+            offset[] = {0,-0.87,-1.74};
+        };
+        class Node8
+        {
+            offset[] = {0,-1.67,-1.74};
+        };
+    };
+};
+class A3_Aegis_Air_F_Aegis_Heli_Transport_02_Heli_Transport_02_VIP_F_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0.39,2.8,-1.74};
+        };
+        class Node2
+        {
+            offset[] = {0.39,2,-1.74};
+        };
+    };
+};
+
+class A3_Aegis_air_f_Aegis_Heli_Attack_04_Heli_Attack_04_F_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0.59,3.95,-2};
+        };
+    };
+};
+class A3_Aegis_Air_F_Aegis_Plane_Transport_01_Plane_Transport_01_F_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.85,-4.55};
+        };
+        class Node2
+        {
+            offset[] = {0,-2.65,-4.55};
+        };
+    };
+};
+class Aegis_I_E_APC_Wheeled_01_medical_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.38,-0.18};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.42,-0.18};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.22,-0.18};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.02,-0.18};
+        };
+        class Node5
+        {
+            offset[] = {0,-2.82,-0.18};
+        };
+        class Node6
+        {
+            offset[] = {0,-3.62,-0.18};
+        };
+        class Node7
+        {
+            offset[] = {0,-4.42,-0.18};
+        };
+    };
+};
+
+class Aegis_B_E_APC_Wheeled_01_medical_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.38,-0.18};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.42,-0.18};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.22,-0.18};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.02,-0.18};
+        };
+        class Node5
+        {
+            offset[] = {0,-2.82,-0.18};
+        };
+        class Node6
+        {
+            offset[] = {0,-3.62,-0.18};
+        };
+        class Node7
+        {
+            offset[] = {0,-4.42,-0.18};
+        };
+    };
+};
+
+class B_APC_Wheeled_01_medical_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.38,-0.18};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.42,-0.18};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.22,-0.18};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.02,-0.18};
+        };
+        class Node5
+        {
+            offset[] = {0,-2.82,-0.18};
+        };
+        class Node6
+        {
+            offset[] = {0,-3.62,-0.18};
+        };
+        class Node7
+        {
+            offset[] = {0,-4.42,-0.18};
+        };
+    };
+};
+
+class B_T_APC_Wheeled_01_medical_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.38,-0.18};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.42,-0.18};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.22,-0.18};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.02,-0.18};
+        };
+        class Node5
+        {
+            offset[] = {0,-2.82,-0.18};
+        };
+        class Node6
+        {
+            offset[] = {0,-3.62,-0.18};
+        };
+        class Node7
+        {
+            offset[] = {0,-4.42,-0.18};
+        };
+    };
+};
+
+class B_W_APC_Wheeled_01_medical_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.38,-0.18};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.42,-0.18};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.22,-0.18};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.02,-0.18};
+        };
+        class Node5
+        {
+            offset[] = {0,-2.82,-0.18};
+        };
+        class Node6
+        {
+            offset[] = {0,-3.62,-0.18};
+        };
+        class Node7
+        {
+            offset[] = {0,-4.42,-0.18};
+        };
+    };
+};
+
+class O_R_APC_Tracked_02_medical_ard_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.22,-0.25};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.02,-0.25};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.82,-0.25};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.62,-0.43};
+        };
+        class Node5
+        {
+            offset[] = {0,-3.42,-0.48};
+        };
+        class Node6
+        {
+            offset[] = {0,-4.22,-0.54};
+        };
+    };
+};
+
+class O_R_APC_Tracked_02_medical_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.22,-0.25};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.02,-0.25};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.82,-0.25};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.62,-0.43};
+        };
+        class Node5
+        {
+            offset[] = {0,-3.42,-0.48};
+        };
+        class Node6
+        {
+            offset[] = {0,-4.22,-0.54};
+        };
+    };
+};
+
+class Atlas_O_W_APC_Tracked_02_medical_F : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.22,-0.25};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.02,-0.25};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.82,-0.25};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.62,-0.43};
+        };
+        class Node5
+        {
+            offset[] = {0,-3.42,-0.48};
+        };
+        class Node6
+        {
+            offset[] = {0,-4.22,-0.54};
+        };
+    };
+};
