a6862794bf4c7942d07071911ac58495defefd56
wersal454 <wroma@bk.ru>
2024-03-06 23:34:07 +0300

    Update WS.hpp

--- A3A/addons/logistics/Cargo/WS.hpp
+++ A3A/addons/logistics/Cargo/WS.hpp
@@ -2,7 +2,7 @@ class lxws_vehicles_f_lxws_zu23_zu23_lxws_p3d : TRIPLES(ADDON,Cargo,Base)
 {
     offset[] = {0,0,1.96};
     rotation[] = {0,1,0};
-    size = 6;
+    size = 5;
     recoil = 1250;
     isWeapon = 1;
 };
@@ -22,9 +22,21 @@ class lxWS_air_1_f_lxWS_UAV_02_Bustard_IED_p3d : TRIPLES(ADDON,Cargo,Base)
     recoil = 500;
     isWeapon = 1;
 };
+class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_unarmed_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {0,0,1.65};
+    rotation[] = {0,1,0};
+    size = 7;
+};
+class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_armor_unarmed_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {0,0,2.1};
+    rotation[] = {0,1,0};
+    size = 7;
+};
 class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_armor_AT_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.1};
     rotation[] = {0,1,0};
     size = 7;
     recoil = 250;
@@ -32,7 +44,7 @@ class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_armor_AT_lxWS_p3d : TRIPLES(ADD
 };
 class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_armor_hmg_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.25};
     rotation[] = {0,1,0};
     size = 7;
     recoil = 250;
@@ -40,7 +52,7 @@ class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_armor_hmg_lxWS_p3d : TRIPLES(AD
 };
 class lxws_vehicles_1_f_lxws_APC_Wheeled_02_APC_Wheeled_02_HMG_lxws_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {-0.24,0,2.65};
     rotation[] = {0,1,0};
     size = 7;
     recoil = 250;
@@ -48,13 +60,13 @@ class lxws_vehicles_1_f_lxws_APC_Wheeled_02_APC_Wheeled_02_HMG_lxws_p3d : TRIPLE
 };
 class lxws_vehicles_1_f_lxws_APC_Wheeled_02_APC_Wheeled_02_unarmed_lxws_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {-0.23,0,2.61};
     rotation[] = {0,1,0};
     size = 7;
 };
 class lxws_vehicles_f_lxws_Truck_02_Truck_02_AA_lxws_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.79};
     rotation[] = {0,1,0};
     size = 8;
     recoil = 1250;
@@ -62,19 +74,19 @@ class lxws_vehicles_f_lxws_Truck_02_Truck_02_AA_lxws_p3d : TRIPLES(ADDON,Cargo,B
 };
 class lxws_vehicles_f_lxws_Truck_02_Truck_02_cargo_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.38};
     rotation[] = {0,1,0};
     size = 8;
 };
 class lxws_vehicles_f_lxws_Truck_02_Truck_02_flatbed_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.38};
     rotation[] = {0,1,0};
     size = 8;
 };
 class lxws_vehicles_f_lxws_APC_Wheeled_01_APC_Wheeled_01_mortar_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.57};
     rotation[] = {0,1,0};
     size = 8;
     recoil = 650;
@@ -82,7 +94,7 @@ class lxws_vehicles_f_lxws_APC_Wheeled_01_APC_Wheeled_01_mortar_lxWS_p3d : TRIPL
 };
 class lxws_vehicles_f_lxws_APC_Wheeled_01_APC_Wheeled_01_command_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.57};
     rotation[] = {0,1,0};
     size = 8;
     recoil = 350;
@@ -90,7 +102,7 @@ class lxws_vehicles_f_lxws_APC_Wheeled_01_APC_Wheeled_01_command_lxWS_p3d : TRIP
 };
 class lxws_vehicles_f_lxws_APC_Wheeled_01_APC_Wheeled_01_atgm_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.57};
     rotation[] = {0,1,0};
     size = 8;
     recoil = 550;
@@ -98,13 +110,13 @@ class lxws_vehicles_f_lxws_APC_Wheeled_01_APC_Wheeled_01_atgm_lxWS_p3d : TRIPLES
 };
 class lxws_vehicles_f_lxws_Truck_02_Truck_02_box_lxWS_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.37};
     rotation[] = {0,1,0};
     size = 8;
 };
 class lxWS_vehicles_1_f_lxws_APC_Tracked_02_APC_Tracked_02_30mm_lxws_p3d : TRIPLES(ADDON,Cargo,Base)
 {
-    offset[] = {0,0,1.8};
+    offset[] = {0,0,2.43};
     rotation[] = {0,1,0};
     size = 9;
     recoil = 550;
