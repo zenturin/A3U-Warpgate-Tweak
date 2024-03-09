6e4a3f053103e3090668ec6236384541622634cc
wersal454 <wroma@bk.ru>
2024-03-09 22:45:54 +0300

    Update cfgPlaneLoadouts.hpp

--- A3A/addons/ultimate/config/plane/cfgPlaneLoadouts.hpp
+++ A3A/addons/ultimate/config/plane/cfgPlaneLoadouts.hpp
@@ -53,10 +53,6 @@ class planeLoadouts
    
     class AA
     {
-        class B_Plane_Fighter_01_F
-        {
-            loadout[] = {"PylonRack_Missile_BIM9X_x2","PylonRack_Missile_BIM9X_x2","PylonRack_Missile_BIM9X_x2","PylonRack_Missile_BIM9X_x2","PylonMissile_Missile_BIM9X_x1","PylonMissile_Missile_BIM9X_x1","PylonMissile_Missile_AMRAAM_D_INT_x1","PylonMissile_Missile_AMRAAM_D_INT_x1","PylonMissile_Missile_AMRAAM_D_INT_x1","PylonMissile_Missile_AMRAAM_D_INT_x1","PylonMissile_Missile_AMRAAM_D_INT_x1","PylonMissile_Missile_AMRAAM_D_INT_x1"};
-        };
         class AMF_RAFALE_B_01_F
         {
             loadout[] = {"PylonMissile_Missile_MICAIR_x1","PylonMissile_Missile_MICAIR_x1","","","PylonRack_Missile_METEOR_1_x1","PylonRack_Missile_METEOR_1_x1","PylonRack_Missile_TANK_02_x1_f","PylonRack_Missile_TANK_02_x1_f","PylonRack_2_Missile_MICAEM_x1","PylonRack_2_Missile_MICAEM_x1","PylonRack_Missile_TANK_01_x1_f"};
