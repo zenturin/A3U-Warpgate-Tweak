86954add9ef78b7f70df2bc3e986a401405ded0f
UAC-MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
2024-03-12 19:20:08 +0200

    main menu update/changes
    Updated:
    random background image chances.
    
    Added:
    Multiple new background images.
    Loading screens on main startup. (only works if client has "Skip logos at start up" Disabled)

--- A3A/addons/main_menu/config.cpp
+++ A3A/addons/main_menu/config.cpp
@@ -23,24 +23,68 @@ class RANDOM_NUMBER
     number = RANDOM_NUMBER;
 };
 
-#if RANDOM_NUMBER > 240
-    #define BACKGROUND data\backgrounds\racs_co.paa
-#endif
-
-#if RANDOM_NUMBER > 200
-    #define BACKGROUND data\backgrounds\pla_co.paa
-#endif
-
-#if RANDOM_NUMBER > 150
-    #define BACKGROUND data\backgrounds\rebels_co.paa
-#endif
-
-#if RANDOM_NUMBER > 100
-    #define BACKGROUND data\backgrounds\canada_co.paa
-#endif
-
-#if RANDOM_NUMBER > 50
+#if RANDOM_NUMBER > 0
     #define BACKGROUND data\backgrounds\us_army_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 17
+    #define BACKGROUND data\backgrounds\absolute_heat_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 34
+    #define BACKGROUND data\backgrounds\bwa_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 51
+    #define BACKGROUND data\backgrounds\canada_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 68
+    #define BACKGROUND data\backgrounds\clone_wars_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 85
+    #define BACKGROUND data\backgrounds\ffaa_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 102
+    #define BACKGROUND data\backgrounds\italy_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 119
+    #define BACKGROUND data\backgrounds\pla_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 136
+    #define BACKGROUND data\backgrounds\racs_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 153
+    #define BACKGROUND data\backgrounds\rebels_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 170
+    #define BACKGROUND data\backgrounds\russia_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 187
+    #define BACKGROUND data\backgrounds\spearhead_1944_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 204
+    #define BACKGROUND data\backgrounds\sweden_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 221
+    #define BACKGROUND data\backgrounds\turkey_co.jpg
+#endif
+
+#if RANDOM_NUMBER > 238
+    #define BACKGROUND data\backgrounds\vietnarm_co.jpg
+#endif
+
+#if RANDOM_NUMBER == 255
+    #define BACKGROUND data\backgrounds\testbg_co.jpg
 #endif
 
 #ifndef BACKGROUND
@@ -49,7 +93,165 @@ class RANDOM_NUMBER
 
 class RscActivePicture;
 class RscStandardDisplay;
-class RscDisplayMain: RscStandardDisplay 
+class RscPicture;
+class RscText;
+class RscStructuredText;
+class RscTitles //loading screen
+{
+	class SplashNoise
+	{
+		idd=-1;
+		movingEnable=0;
+		duration=11;
+		fadein=0;
+		fadeout=0;
+		controls[]=
+		{
+			"BackgroundNoise"
+		};
+		class BackgroundNoise: RscPicture
+		{
+			text = QPATHTOFOLDER(data\backgrounds\AUC_Credits_Blank_co.jpg);
+			colorText[]={1,1,1,1};
+			x="safezoneX";
+			y="safezoneY";
+			w="safezoneW";
+			h="safezoneH";
+		};
+	};
+	class SplashBohemia
+	{
+		idd=-1;
+		movingEnable=0;
+		duration=4;
+		fadein=0;
+		fadeout=0;
+		name="$STR_A3_SplashCopyright_0";
+		controls[]=
+		{
+			"PictureA3Logo",
+			"PictureBIlogo",
+			"PicturePhysXLogo",
+			"LegalText"
+		};
+		class PictureA3Logo: RscPicture
+		{
+			idc=1200;
+			text="\A3\Ui_f\data\Logos\arma3_white_ca.paa";
+			x="SafeZoneX + SafeZoneW / 2 - (36 * (0.01875 * SafezoneH)) / 2";
+			y="SafeZoneY +	SafeZoneH / 2 - (13.7 * (0.025 * SafezoneH)) / 2";
+			w="8 * (0.01875 * SafezoneH)";
+			h="4 * (0.025 * SafezoneH)";
+		};
+		class PictureBIlogo: RscPicture
+		{
+			idc=1201;
+			text="\A3\Ui_f\data\IGUI\RscTitles\SplashBohemia\bi_logo_small_ca.paa";
+			x="SafeZoneX + SafeZoneW / 2 - (33 * (0.01875 * SafezoneH)) / 2";
+			y="SafeZoneY +	SafeZoneH / 2 - (25.7 * (0.025 * SafezoneH)) / 2";
+			w="32 * (0.01875 * SafezoneH)";
+			h="16 * (0.025 * SafezoneH)";
+		};
+		class PicturePhysXLogo: RscPicture
+		{
+			idc=1202;
+			text="\A3\Ui_f\data\Logos\GameWorks_white_ca.paa";
+			x="SafeZoneX + SafeZoneW / 2 + (16.5 * (0.01875 * SafezoneH)) / 2";
+			y="SafeZoneY +	SafeZoneH / 2 - (13.6 * (0.025 * SafezoneH)) / 2";
+			w="16 * (0.01875 * SafezoneH)";
+			h="4 * (0.025 * SafezoneH)";
+		};
+		class LegalText: RscStructuredText
+		{
+			idc=-1;
+			text="$STR_A3_LEGAL_SPLASH";
+			size="0.02 * SafezoneH";
+			shadow=0;
+			x="SafeZoneX + SafeZoneW / 2 - (50 * (0.01875 * SafezoneH)) / 2";
+			y="SafeZoneY +	SafeZoneH - (0.38 * SafezoneH)";
+			w="50 * (0.01875 * SafezoneH)";
+			h="0.39 * SafezoneH";
+			class Attributes
+			{
+				align="center";
+				shadow=0;
+			};
+		};
+	};
+	class SplashArma3
+	{
+		idd=-1;
+		movingEnable=0;
+		duration=4;
+		fadein=1;
+		fadeout=1;
+		name="$STR_A3_SplashBohemia_0";
+		controls[]=
+		{
+			"Picture",
+			"TextPresents"
+		};
+		class Picture: RscPicture
+		{
+			text = QPATHTOFOLDER(data\backgrounds\AUC_Credits_co.jpg);
+			colorText[]={1,1,1,1};
+			x="safezoneX";
+			y="safezoneY";
+			w="safezoneW";
+			h="safezoneH";
+		};
+		class TextPresents: RscText
+		{
+			idc=1000;
+			text="";
+			style=2;
+			sizeEx="0.03*SafezoneH";
+			shadow=0;
+			x="SafeZoneX + SafeZoneW / 2 - (46.5 * 		(0.01875 * SafezoneH)) / 2";
+			y="SafeZoneY +	SafeZoneH - (0.505 * SafezoneH)";
+			w="48 * 		(0.01875 * SafezoneH)";
+			h="0.03 * SafezoneH";
+		};
+	};
+	class SplashCopyright
+	{
+		idd=-1;
+		movingEnable=0;
+		duration=2;
+		fadein=0;
+		fadeout=2;
+		name="-";
+		controls[]=
+		{
+			"TextPresents"
+		};
+		class TextPresents: RscText
+		{
+			idc=1000;
+			text="Loading...";
+			style=2;
+			sizeEx="0.03*SafezoneH";
+			shadow=0;
+			x = "safeZoneX + (safeZoneW / 2) - ((48 * (0.01875 * safeZoneH)) / 2)"; // Center horizontally
+            y = "safeZoneY + safeZoneH - (0.03 * safeZoneH)"; // Bottom of the screen
+			w="48 * (0.01875 * SafezoneH)";
+			h="0.03 * SafezoneH";
+		};
+	};
+	class SplashESRB
+	{
+		onLoad = "(_this # 0) ctrlEnable false;";
+	};
+	class SplashTagline
+	{
+		onLoad = "(_this # 0) ctrlEnable false;";
+	};
+	class RscCredits
+	{
+		onLoad = "(_this # 0) ctrlEnable false;";
+	};
+};
+class RscDisplayMain: RscStandardDisplay //main menu
 {
 	enableDisplay = 0;
 	class controlsBackground 
@@ -82,16 +284,16 @@ class RscDisplayMain: RscStandardDisplay
 		delete SpotlightNext;
 		delete SpotlightPrev;
 		class Logo: RscActivePicture
-		{ //change to discord
-			text = QPATHTOFOLDER(data\AUC_Logo.paa);
+		{
+			text = QPATHTOFOLDER(data\AULOGO_TEST.paa);
 			tooltip="Join the official Antistasi Ultimate Discord server!";
 			color[]={0.89999998,0.89999998,0.89999998,1};
 			colorActive[]={1,1,1,1};
 			shadow=0;
 			x="0.5 - 	5 * 	(pixelW * pixelGrid * 2)";
-			y="safezoneY + (3 - 0.5 * 	5) * 	(pixelH * pixelGrid * 2)";
+			y="safezoneY + (2.75 - 0.25 * 	5) * 	(pixelH * pixelGrid * 2)";
 			w="2 * 	5 * 	(pixelW * pixelGrid * 2)";
-			h="1 * 	5 * 	(pixelH * pixelGrid * 2)";
+			h="0.5 * 	5 * 	(pixelH * pixelGrid * 2)";
 			url = "https://discord.gg/antistasiultimate";
 			onButtonClick="['https://discord.gg/antistasiultimate'] call bis_fnc_executeCode;";
 			onSetFocus="(_this select 0) ctrlsettextcolor [1,1,1,1];";
@@ -104,18 +306,18 @@ class RscDisplayMain: RscStandardDisplay
 			type = 1;
 			idc = -1;
 			style = 48;
-			x = "safeZoneX + safeZoneW - 0.18"; // Adjusted x-coordinate to place it at the bottom right of the screen
-			y = "safeZoneY + safeZoneH - 0.27"; // Adjusted y-coordinate to place it at the bottom right of the screen
+			x = "safeZoneX + safeZoneW - 0.18";
+			y = "safeZoneY + safeZoneH - 0.27";
 			w = 0.18;
 			h = 0.05;
 			text = QPATHTOFOLDER(data\DevB.paa); // No text needed for an image button
-			colorBackground[] = {0, 0, 0, 0}; // Transparent background
+			colorBackground[] = {0, 0, 0, 0};
 			colorText[] = {1, 1, 1, 1};
 			font = "RobotoCondensed";
 			sizeEx = 0.04;
-			colorFocused[] = {1, 1, 1, 0.9}; // Background color when focused (not used)
-			colorShadow[] = {0, 0, 0, 0}; // Shadow color (not used)
-			colorBorder[] = {0, 0, 0, 0}; // Border color (not used)
+			colorFocused[] = {1, 1, 1, 0.9};
+			colorShadow[] = {0, 0, 0, 0};
+			colorBorder[] = {0, 0, 0, 0};
 			shadow = 0;
 			soundEnter[] = {"", 0.1, 1};
 			soundPush[] = {"", 0.1, 1};
@@ -137,46 +339,46 @@ class RscDisplayMain: RscStandardDisplay
 			text = QPATHTOFOLDER(data\DevB.paa); // Path to button image
 			tooltip = "Steam workshop Mod";
 			url = "https://steamcommunity.com/sharedfiles/filedetails/?id=3020755032";
-			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB_Hov.paa"""; // Change button image when hovered
-			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB.paa"""; // Change button image when unhovered
-			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB_Hov.paa"""; // Change button image when hovered
-			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB.paa"""; // Change button image when unhovered
+			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB_Hov.paa""";
+			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB.paa""";
+			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB_Hov.paa""";
+			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\DevB.paa""";
 		};
 		class GitHubButton2: base_side_btn 
         {
 			onLoad = "(_this # 0) ctrlEnable true";
-			y = "safeZoneY + safeZoneH - 0.33"; // Adjusted y-coordinate to place it at the bottom right of the screen
+			y = "safeZoneY + safeZoneH - 0.33";
 			text = QPATHTOFOLDER(data\GitB.paa); // Path to button image
 			tooltip="GitHub Page";
 			url = "https://github.com/SilenceIsFatto/A3-Antistasi-Ultimate";
-			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB_Hov.paa"""; // Change button image when hovered
-			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB.paa"""; // Change button image when unhovered
-			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB_Hov.paa"""; // Change button image when hovered
-			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB.paa"""; // Change button image when unhovered
+			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB_Hov.paa""";
+			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB.paa""";
+			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB_Hov.paa""";
+			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\GitB.paa""";
 		};
 		class ServerButton3: base_side_btn 
         {
 			onLoad = "(_this # 0) ctrlEnable true";
-			y = "safeZoneY + safeZoneH - 0.39"; // Adjusted y-coordinate to place it at the bottom right of the screen
+			y = "safeZoneY + safeZoneH - 0.39";
 			text = QPATHTOFOLDER(data\ServerB.paa); // No text needed for an image button
 			tooltip="Connect to our community server!";
 			onMouseButtonClick = "connectToServer ['stutpip123.ddns.net', 2302, '1221']";
-			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB_Hov.paa"""; // Change button image when hovered
-			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB.paa"""; // Change button image when unhovered
-			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB_Hov.paa"""; // Change button image when hovered
-			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB.paa"""; // Change button image when unhovered
+			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB_Hov.paa""";
+			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB.paa""";
+			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB_Hov.paa""";
+			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ServerB.paa""";
 		};
 		class ArmaCreditsButton4: base_side_btn 
         {
 			onLoad = "(_this # 0) ctrlEnable true";
-			x = "safeZoneX"; // Adjusted x-coordinate to place it at the bottom right of the screen
+			x = "safeZoneX";
 			text = QPATHTOFOLDER(data\ArmaCredits.paa); // No text needed for an image button
 			tooltip="ArmA 3 Credits";
 			onMouseButtonClick = "if (scriptdone (missionnamespace getvariable ['RscDisplayMain_credits',scriptnull])) then {RscDisplayMain_credits = _this spawn (uinamespace getvariable 'bis_fnc_credits');};";
-			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits_Hov.paa"""; // Change button image when hovered
-			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits.paa"""; // Change button image when unhovered
-			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits_Hov.paa"""; // Change button image when hovered
-			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits.paa"""; // Change button image when unhovered
+			onMouseEnter = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits_Hov.paa""";
+			onMouseExit = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits.paa""";
+			onMouseButtonUp = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits_Hov.paa""";
+			onMouseButtonDown = "(_this select 0) ctrlSetText ""\x\a3a\addons\main_menu\data\ArmaCredits.paa""";
 		};
 	};
 };
