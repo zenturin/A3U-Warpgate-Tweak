a6a7273e024d80216e80baa9c9698ea9609cadf9
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2024-03-19 16:13:01 +0000

    update validation tool

--- Tools/validate_antistasi.ps1
+++ Tools/validate_antistasi.ps1
@@ -1,2 +1,2 @@
-python .\sqfvalidator\sqflint.py -d "..\A3-Antistasi" | Tee-Object -FilePath ".\ValidationResults.txt"
+python .\sqfvalidator\sqflint.py -d "..\A3A\addons\core\Templates\Templates\Vanilla" | Tee-Object -FilePath ".\ValidationResults.txt"
 Read-Host -Prompt "Press Enter to exit"
