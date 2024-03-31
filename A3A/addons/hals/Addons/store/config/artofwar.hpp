ca62e1fcd391086380d4056bdf212eeb582220ac
wersal454 <wroma@bk.ru>
2024-03-31 07:24:20 +0300

    split vanilla store + additions + kkiv mod support

---
+++ A3A/addons/hals/Addons/store/config/artofwar.hpp
@@ -0,0 +1,32 @@
+		class miscArtofwar 
+		{
+			displayName = __EVAL(formatText ["%1 %2", localize "STR_A3AU_Artofwar", localize "STR_A3AU_misc"]);
+			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\backpack_ca.paa";
+
+			ITEM(U_C_FormalSuit_01_black_F, 1000, MISC_STOCK);
+			ITEM(U_C_FormalSuit_01_blue_F, 1000, MISC_STOCK);
+			ITEM(U_C_FormalSuit_01_gray_F, 1000, MISC_STOCK);
+			ITEM(U_C_FormalSuit_01_khaki_F, 1000, MISC_STOCK);
+
+			ITEM(U_C_FormalSuit_01_tshirt_black_F, 1000, MISC_STOCK);
+			ITEM(U_C_FormalSuit_01_tshirt_gray_F, 1000, MISC_STOCK);
+
+			ITEM(U_I_ParadeUniform_01_AAF_decorated_F, 2000, MISC_STOCK);
+			ITEM(U_O_ParadeUniform_01_CSAT_decorated_F, 2000, MISC_STOCK);
+			ITEM(U_I_E_ParadeUniform_01_LDF_decorated_F, 2000, MISC_STOCK);
+			ITEM(U_B_ParadeUniform_01_US_decorated_F, 2000, MISC_STOCK);
+
+			ITEM(H_ParadeDressCap_01_AAF_F, 500, MISC_STOCK);
+			ITEM(H_ParadeDressCap_01_CSAT_F, 500, MISC_STOCK);
+			ITEM(H_ParadeDressCap_01_LDF_F, 500, MISC_STOCK);
+			ITEM(H_ParadeDressCap_01_US_F, 500, MISC_STOCK);
+
+			ITEM(B_CivilianBackpack_01_Everyday_Astra_F, 500, MISC_STOCK);
+			ITEM(B_CivilianBackpack_01_Everyday_Black_F, 500, MISC_STOCK);
+			ITEM(B_CivilianBackpack_01_Everyday_Vrana_F, 500, MISC_STOCK);
+			ITEM(B_CivilianBackpack_01_Everyday_IDAP_F, 500, MISC_STOCK);
+			ITEM(B_CivilianBackpack_01_Sport_Blue_F, 500, MISC_STOCK);
+			ITEM(B_CivilianBackpack_01_Sport_Green_F, 500, MISC_STOCK);
+			ITEM(B_CivilianBackpack_01_Sport_Red_F, 500, MISC_STOCK);
+
+		};
