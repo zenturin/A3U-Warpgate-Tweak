d60dd7f516d9378e948bea31ac2197ae17183e15
wersal454 <wroma@bk.ru>
2024-04-03 16:06:27 +0300

    CLSA cargo

---
+++ A3A/addons/logistics/Cargo/CLSA.hpp
@@ -0,0 +1,64 @@
+class csla_afmc_weapons_M252_afmc_81Mortar_p3d : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {-0.1,-0.5,0.74};
+    rotation[] = {0,1,0};
+    size = 2;
+    recoil = 2000;
+    isWeapon = 1;
+    blackList[] = {"\A3\boat_f_gamma\Boat_Civil_01\Boat_Civil_01_F","\A3\boat_f\Boat_Transport_01\Boat_Transport_01_F.p3d","\A3\Boat_F_Exp\Boat_Transport_02\Boat_Transport_02_F.p3d"};
+};
+
+class csla_weapons_m52_m52_komplet_p3d : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {-0.1,-0.5,0.74};
+    rotation[] = {0,1,0};
+    size = 2;
+    recoil = 2000;
+    isWeapon = 1;
+    blackList[] = {"\A3\boat_f_gamma\Boat_Civil_01\Boat_Civil_01_F","\A3\boat_f\Boat_Transport_01\Boat_Transport_01_F.p3d","\A3\Boat_F_Exp\Boat_Transport_02\Boat_Transport_02_F.p3d"};
+};
+
+class csla_afmc_weapons_m2_m2_static_HIGH : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {0.2,-0.5,1.69};
+    rotation[] = {0,1,0};
+    size = 4;
+    recoil = 100;
+    isWeapon = 1;
+};
+
+class csla_afmc_weapons_m2_m2_static_low: TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {0,0,1.24};
+    rotation[] = {0,-1,0};
+    size = 4;
+    recoil = 100;
+    isWeapon = 1;
+};
+
+class csla_afmc_weapons_tow_tow_static : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {-0.1,-0.5,0.74};
+    rotation[] = {0,1,0};
+    size = 2;
+    recoil = 2000;
+    isWeapon = 1;
+};
+
+class csla_artillery_pldvk_pldvk_m59_kanon_p3d : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {0,0,1.96};
+    rotation[] = {0,1,0};
+    size = 5;
+    recoil = 1250;
+    isWeapon = 1;
+};
+
+class csla_weapons_t21_t21_static : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {-0.1,-0.5,0.74};
+    rotation[] = {0,1,0};
+    size = 2;
+    recoil = 2000;
+    isWeapon = 1;
+};
