31f59355e0bab0e0c883b03824e0710226fe4330
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2024-04-06 18:56:11 +0100

    FoW Enable

--- A3A/addons/core/Templates/Templates/FoW/templates.hpp
+++ A3A/addons/core/Templates/Templates/FoW/templates.hpp
@@ -1,8 +1,8 @@
     class FOW_Base
     {
         requiredAddons[] = {"IFA3_Core","fow_tanks","IFA3_FOW_Compat"};
-        basepath = QPATHTOFOLDER(Templates\Templates\IFA);
-        logo = QPATHTOFOLDER(Templates\Templates\IFA\ifa_logo.paa);
+        basepath = QPATHTOFOLDER(Templates\Templates\FoW);
+        logo = QPATHTOFOLDER(Templates\Templates\FoW\fow_logo.paa);
         priority = 15;
         equipFlags[] = {"lowTech"};
     };
@@ -11,17 +11,15 @@
     {
         side = "Occ";
         flagTexture = "ww2\core_t\if_decals_t\german\flag_ger_co.paa";
-        name = "IFA WEH Temperate";
-        file = "IFA_AI_WEH_Temperate";
-        climate[] = {"temperate","tropical"};
+        name = "FoW WEH Temperate";
+        file = "FOW_AI_WEH_Temperate";
     };
 	
     class FOW_USA_Temperate : FOW_Base
     {
         side = "Inv";
         flagTexture = "a3\data_f\flags\flag_us_co.paa";
-        name = "IFA USA Temperate";
-        file = "IFA_AI_USA_Temperate";
-        climate[] = {"temperate","tropical"};
+        name = "FoW USA Temperate";
+        file = "FOW_AI_USA_Temperate";
     };		
 
